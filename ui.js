74732aa161bea5ea55e96a76d1ff27fa20defd48
Tobias <tobias.reichel@hs-augsburg.de>
2020-05-28 15:35:21 +0200

    init

---
+++ ui.js
@@ -0,0 +1,77 @@
+/*
+    * MQTT-WebClient example for Web-IO 4.0
+*/
+var hostname = "test.mosquitto.org";
+var clientId = "mqttJs";
+var port = 8081;
+clientId += new Date().getUTCMilliseconds();;
+var subscription = "/$_fluzzy$$cadabra/Nö/Heizung";
+
+mqttClient = new Paho.MQTT.Client(hostname, port, clientId);
+mqttClient.onMessageArrived = MessageArrived;
+mqttClient.onConnectionLost = ConnectionLost;
+Connect();
+
+function Connect(){
+    mqttClient.connect({
+    onSuccess: Connected,
+    onFailure: ConnectionFailed});
+}
+
+function Connected() {
+    console.log("Connected");
+    mqttClient.subscribe(subscription);
+}
+
+function ConnectionFailed(res) {
+    console.log("Connect failed:" + res.errorMessage);
+}
+
+function ConnectionLost(res) {
+    if (res.errorCode !== 0) {
+        console.log("Connection lost:" + res.errorMessage);
+        Connect();
+    }
+}
+
+/*Callback for incoming message processing */
+function MessageArrived(message) {
+    console.log(message.destinationName +" : " + message.payloadString);
+    switch(message.payloadString){
+        case "ON":
+            displayClass = "on";
+            break;
+        case "OFF":
+            displayClass = "off";
+            break;
+        default:
+            displayClass = "unknown";
+    }
+    var topic = message.destinationName.split("/");
+    if (topic.length == 3){
+        var ioname = topic[1];
+        UpdateElement(ioname, displayClass);
+    }
+}
+
+function Send(){
+    var low = document.getElementById('low').value;
+    var high = document.getElementById('high').value;
+
+    if(low>high){
+        alert('Low higher then High!')
+    } else{
+        var message = new Paho.MQTT.Message(low+';'+high);
+        message.destinationName = "/$_fluzzy$$cadabra/Nö/Heizung";
+        mqttClient.send(message);
+    }
+    
+}
+
+function DragEnd(){
+    var low = document.getElementById('low').value;
+    var high = document.getElementById('high').value;
+
+    document.getElementById('lblLow').innerText = 'Low: ' + low;
+    document.getElementById('lblHigh').innerText = 'High: ' + high;
+}
